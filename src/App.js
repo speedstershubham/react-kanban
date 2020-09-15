5ee564fb487e79ebe8bafa9a91f5cd50cf3125a3
Shubham Chowksay <shubhamchowksay10@gmail.com>
2020-09-15 15:30:50 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,51 +1,83 @@
-import React, { useState , useEffect } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Board, { moveCard } from "@lourenci/react-kanban";
-import axios from 'axios';
 import "@lourenci/react-kanban/dist/styles.css";
 // Use your own styles to override the default styles
-import "./styles.css";
-//import DataFetching from "./datafetch";
+// import "./styles.css";
+import DataFetching from "./datafetch"
 
-
-const DataFetching = () => {
- // const [loading,setloading] = useState(true)
- // const [error,seterror] = useState('')
-  const [posts,setpost] = useState([])
-
-  useEffect(() =>{
-axios.get("https://cors-anywhere.herokuapp.com/https://react-kanban-server.herokuapp.com/")
-.then(res =>{
-  console.log(res)
-//  setloading(false)
-  setpost(res.data)
- // seterror('')
-})
-.catch( error =>{
- // setloading(false)
-  setpost({})
- // seterror('')
-})
-  }, [])
-
-  return(
-      
-      <div>
-          <ul>
-          {posts.map(post =>(
-          <li key={post.id}>{post.Name}</li>
-))}
-          </ul>
-      
-
-      </div>
-  )
-}
-
+const board = {
+  columns: [
+    {
+      id: 1,
+      title: "Backlog",
+      cards: [
+        {
+          id: 1,
+          title: "Card title 1",
+          description: "Card content"
+        },
+        {
+          id: 2,
+          title: "Card title 2",
+          description: "Card content"
+        },
+        {
+          id: 3,
+          title: "Card title 3",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: "Doing",
+      cards: [
+        {
+          id: 9,
+          title: "Card title 9",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: "Q&A",
+      cards: [
+        {
+          id: 10,
+          title: "Card title 10",
+          description: "Card content"
+        },
+        {
+          id: 11,
+          title: "Card title 11",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: "Production",
+      cards: [
+        {
+          id: 12,
+          title: "Card title 12",
+          description: "Card content"
+        },
+        {
+          id: 13,
+          title: "Card title 13",
+          description: "Card content"
+        }
+      ]
+    }
+  ]
+};
 
 function ControlledBoard() {
   // You need to control the state yourself.
-  const [controlledBoard, setBoard] = useState(DataFetching);
+  const [controlledBoard, setBoard] = useState(board);
 
   function handleCardMove(_card, source, destination) {
     const updatedBoard = moveCard(controlledBoard, source, destination);
@@ -56,22 +88,47 @@ function ControlledBoard() {
     <Board onCardDragEnd={handleCardMove} disableColumnDrag>
       {controlledBoard}
     </Board>
-     
-  ); 
+  );
+}
+
+function UncontrolledBoard() {
+  return (
+    <Board
+      allowRemoveLane
+      allowRenameColumn
+      allowRemoveCard
+      onLaneRemove={console.log}
+      onCardRemove={console.log}
+      onLaneRename={console.log}
+      initialBoard={board}
+      allowAddCard={{ on: "top" }}
+      onNewCardConfirm={draftCard => ({
+        id: new Date().getTime(),
+        ...draftCard
+      })}
+      onCardNew={console.log}
+    />
+  );
 }
 
 function App() {
+ 
   return (
+    
     <>
-      
+      <h4>Example of an uncontrolled board</h4>
+      <UncontrolledBoard />
+      <h4>Example of a controlled board</h4>
       <p>Just the card moving is implemented in this demo.</p>
       <p>
         In this kind of board, you can do whatever you want. We just mirror your
         board state.
       </p>
       <ControlledBoard />
-  
+
+      <DataFetching  />
     </>
   );
 }
+
 export default App;
--- src/datafetch.js
+++ src/datafetch.js
@@ -1,22 +1,23 @@
 import React , {useState,useEffect} from 'react'
 import axios from 'axios';
 
-const DataFetching = () => {
+const DataFetching = props => {
+    console.log(props)
     const [loading,setloading] = useState(true)
     const [error,seterror] = useState('')
-    const [posts,setpost] = useState([])
+    const [columns,setcolumns] = useState([])
 
     useEffect(() =>{
 axios.get("https://cors-anywhere.herokuapp.com/https://react-kanban-server.herokuapp.com/")
 .then(res =>{
     console.log(res)
     setloading(false)
-    setpost(res.data)
+    setcolumns(res.data.columns)
     seterror('')
 })
 .catch( error =>{
     setloading(false)
-    setpost({})
+    setcolumns([])
     seterror('')
 })
     }, [])
@@ -24,9 +25,11 @@ axios.get("https://cors-anywhere.herokuapp.com/https://react-kanban-server.herok
     return(
         
         <div>
+             {props.columns}
             <ul>
-            {posts.map(post =>(
-            <li key={post.id}>{post.Name}</li>
+            {columns.map((post) =>(
+            <li key={post.id}>{post.title} {post.cards}</li>
+           
 ))}
             </ul>
         
