3ae958d852c5b80f74949919f33f14683242a095
Shubham Chowksay <shubhamchowksay10@gmail.com>
2020-09-16 12:59:51 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -3,106 +3,39 @@ import axios from 'axios';
 import ReactDOM from "react-dom";
 import Board, { moveCard } from "@lourenci/react-kanban";
 import "@lourenci/react-kanban/dist/styles.css";
-// Use your own styles to override the default styles
-// import "./styles.css";
-//import DataFetching from "./datafetch"
+import {board} from "./datafetch"
+
+const datafetch = () =>{
+
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadAllProduct = () => {
+    board().then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProducts(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadAllProduct();
+  }, []);
+ 
+
+      return (
+       
+      )
+}
 
 
-const board = {
-  columns: [
-    {
-      id: 1,
-      title: "Backlog",
-      cards: [
-        {
-          id: 1,
-          title: "Card title 1",
-          description: "Card content"
-        },
-        {
-          id: 2,
-          title: "Card title 2",
-          description: "Card content"
-        },
-        {
-          id: 3,
-          title: "Card title 3",
-          description: "Card content"
-        }
-      ]
-    },
-    {
-      id: 2,
-      title: "Doing",
-      cards: [
-        {
-          id: 9,
-          title: "Card title 9",
-          description: "Card content"
-        }
-      ]
-    },
-    {
-      id: 3,
-      title: "Q&A",
-      cards: [
-        {
-          id: 10,
-          title: "Card title 10",
-          description: "Card content"
-        },
-        {
-          id: 11,
-          title: "Card title 11",
-          description: "Card content"
-        }
-      ]
-    },
-    {
-      id: 4,
-      title: "Production",
-      cards: [
-        {
-          id: 12,
-          title: "Card title 12",
-          description: "Card content"
-        },
-        {
-          id: 13,
-          title: "Card title 13",
-          description: "Card content"
-        }
-      ]
-    }
-  ]
-};
+const ControlledBoard =  () =>{
+ 
+  const [controlledBoard, setBoard] = useState(board);
 
-
-
-function ControlledBoard() {
-  const [loading,setloading] = useState(true)
-  const [error,seterror] = useState('')
-  const [columns,setcolumns] = useState([])
-
-  useEffect(() =>{
-axios.get("https://cors-anywhere.herokuapp.com/https://react-kanban-server.herokuapp.com/")
-.then(res =>{
-  console.log(res)
-  setloading(false)
-  setcolumns(res.data.columns)
-  seterror('')
-})
-.catch( error =>{
-  setloading(false)
-  setcolumns({})
-  seterror('')
-})
-  }, [])
- console.log(columns)
-  // You need to control the state yourself.
-  const [controlledBoard, setBoard] = useState({columns});
-
-  function handleCardMove(_card, source, destination) {
+  const handleCardMove = (_card, source, destination) => {
     const updatedBoard = moveCard(controlledBoard, source, destination);
     setBoard(updatedBoard);
   }
@@ -114,39 +47,14 @@ axios.get("https://cors-anywhere.herokuapp.com/https://react-kanban-server.herok
   );
 }
 
-function UncontrolledBoard() {
-  return (
-    <Board
-      allowRemoveLane
-      allowRenameColumn
-      allowRemoveCard
-      onLaneRemove={console.log}
-      onCardRemove={console.log}
-      onLaneRename={console.log}
-      initialBoard={board}
-      allowAddCard={{ on: "top" }}
-      onNewCardConfirm={draftCard => ({
-        id: new Date().getTime(),
-        ...draftCard
-      })}
-      onCardNew={console.log}
-    />
-  );
-}
 
-function App() {
+
+const App = () => {
  
   return (
     
     <>
-      <h4>Example of an uncontrolled board</h4>
-      <UncontrolledBoard />
-      <h4>Example of a controlled board</h4>
-      <p>Just the card moving is implemented in this demo.</p>
-      <p>
-        In this kind of board, you can do whatever you want. We just mirror your
-        board state.
-      </p>
+      
       <ControlledBoard />
 
   
