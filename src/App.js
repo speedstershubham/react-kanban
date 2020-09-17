74776036fdd821ad3d350368453b6b7535cf1c95
Shubham Chowksay <shubhamchowksay10@gmail.com>
2020-09-17 18:06:22 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,41 +1,107 @@
-import React , {useState,useEffect} from 'react'
-import axios from 'axios';
+import React, {Component, useState } from "react";
 import ReactDOM from "react-dom";
 import Board, { moveCard } from "@lourenci/react-kanban";
 import "@lourenci/react-kanban/dist/styles.css";
-import {board} from "./datafetch"
+ //import "./styles.css";
+import Datafetch from "./Datafetch";
 
-const datafetch = () =>{
+export class App extends Component {
+  state ={message:""}
 
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(false);
-
-  const loadAllProduct = () => {
-    board().then(data => {
-      if (data.error) {
-        setError(data.error);
-      } else {
-        setProducts(data);
-      }
-    });
-  };
-
-  useEffect(() => {
-    loadAllProduct();
-  }, []);
- 
-
-      return (
-       
-      )
+  callbackFunction = (childData) => {
+    this.setState({message: childData})
 }
 
+  render() {
+    
+    return (
+      <div>
+         <ControlledBoard  board={this.state.message}/>
+      <Datafetch Data = {this.callbackFunction}/>
+      {console.log(this.state.message)}
+      <p>  {this.state.message} </p>
+      </div>
+    )
+  }
+}
 
-const ControlledBoard =  () =>{
- 
+export default App
+
+
+const board = {
+  columns: [
+    {
+      id: 1,
+      title: "Backlog",
+      cards: [
+        {
+          id: 1,
+          title: "Card title 1",
+          description: "Card content"
+        },
+        {
+          id: 2,
+          title: "Card title 2",
+          description: "Card content"
+        },
+        {
+          id: 3,
+          title: "Card title 3",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: "Doing",
+      cards: [
+        {
+          id: 9,
+          title: "Card title 9",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: "Q&A",
+      cards: [
+        {
+          id: 10,
+          title: "Card title 10",
+          description: "Card content"
+        },
+        {
+          id: 11,
+          title: "Card title 11",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: "Production",
+      cards: [
+        {
+          id: 12,
+          title: "Card title 12",
+          description: "Card content"
+        },
+        {
+          id: 13,
+          title: "Card title 13",
+          description: "Card content"
+        }
+      ]
+    }
+  ]
+};
+
+const ControlledBoard = () => {
+
   const [controlledBoard, setBoard] = useState(board);
 
-  const handleCardMove = (_card, source, destination) => {
+  function handleCardMove(_card, source, destination) {
     const updatedBoard = moveCard(controlledBoard, source, destination);
     setBoard(updatedBoard);
   }
@@ -48,19 +114,3 @@ const ControlledBoard =  () =>{
 }
 
 
-
-const App = () => {
- 
-  return (
-    
-    <>
-      
-      <ControlledBoard />
-
-  
-    </>
-  );
-}
-
-
-export default App;
--- src/datafetch.js
+++ src/datafetch.js
@@ -1,8 +1,43 @@
-export const board = () => {
-    return fetch(`"https://cors-anywhere.herokuapp.com/https://react-kanban-server.herokuapp.com/"`, { method: "GET" })
-      .then(response => {
-        return response.json();
+import React, { Component , useState , useEffect} from 'react'
+import axios from 'axios'
+
+
+class Datafetch extends Component {
+ sendData = () => {
+    this.props.parentCallback("");
+}
+  render() {
+    function Data() {
+      const[posts, setPost] = useState([])
+    
+      useEffect(() => {
+        axios
+     .get("https://react-kanban-server.herokuapp.com/")
+      .then(response =>{
+        console.log(response)
+        setPost(response.data)
       })
-      .catch(err => console.log(err));
-  };
-  
+      .catch(err => {
+        console.log(err)
+      })
+      },[])
+     
+      return(
+        <div>
+          <ul>
+            {posts.map(post =>(
+              <li key={post.id}>{post.title}</li>
+            ))}
+          </ul>
+        </div>
+      )
+    }
+    return (
+      <div>
+        <Data  />
+      </div>
+    )
+  }
+}
+
+export default Datafetch
