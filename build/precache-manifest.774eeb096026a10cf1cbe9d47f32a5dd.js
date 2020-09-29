b62b43644e1821eae2e176316566dcaa31aa177a
Shubham Chowksay <shubhamchowksay10@gmail.com>
2020-09-29 10:00:49 +0530

    first commit

---
+++ build/precache-manifest.774eeb096026a10cf1cbe9d47f32a5dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee510805af3086a65c9c56aafdc37741",
+    "url": "/index.html"
+  },
+  {
+    "revision": "495a6422a2091c7f48b8",
+    "url": "/static/css/2.4a1c775f.chunk.css"
+  },
+  {
+    "revision": "495a6422a2091c7f48b8",
+    "url": "/static/js/2.1014919f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1014919f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18fa42cfedffcc662d99",
+    "url": "/static/js/main.2b9bfc26.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
